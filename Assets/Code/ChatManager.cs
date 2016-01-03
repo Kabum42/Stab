9658164849ee0b5eaf311a0fd00f30599dca426b
Kabum42 <kabum42@gmail.com>
2016-01-03 04:55:11 +0100

    Chat almost fully working

--- Assets/Code/ChatManager.cs
+++ Assets/Code/ChatManager.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class ChatManager {
 
+	private GameObject physicalChat;
 	public List<ChatMessage> listMessages = new List<ChatMessage>();
 	private int maxMessages = 100;
 
-	public ChatManager() {
+	public ChatManager(GameObject auxPhysicalChat) {
+
+		physicalChat = auxPhysicalChat;
 
 	}
 
@@ -18,9 +22,27 @@ public class ChatManager {
 
 		if (listMessages.Count > maxMessages) {
 			// HERE WOULD BE HANDLED AN OVERFLOW OF MESSAGES
-			// listMessages[0].root.Destroy();
 			listMessages.RemoveAt(0);
 		}
+
+	}
+
+	public void Update() {
+
+		string aux = "";
+
+		for (int i = 0; i < listMessages.Count; i++) {
+
+			aux += "<color=#0000FF>["+listMessages[i].owner+"]</color> : ";
+			aux += listMessages[i].text;
+
+			if (i < listMessages.Count -1) {
+				aux += "\n";
+			}
+
+		}
+
+		physicalChat.GetComponent<Text> ().text = aux;
 
 	}
 
--- Assets/Code/GameScript.cs
+++ Assets/Code/GameScript.cs
@@ -1,29 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameScript : MonoBehaviour {
 
-	private ChatManager chatManager = new ChatManager();
+	private ChatManager chatManager;
+	private GameObject chatInputField;
+	private bool lastTimeChatInputFocused = false;
 
 	// Use this for initialization
 	void Start () {
 
 		// AQUI HAY QUE PASARLE AL CHATMANAGER EL GAMEOBJECT QUE ES EL CONTENT DONDE IRAN LOS MENSAJES
+		chatManager = new ChatManager(GameObject.Find ("Canvas/ChatPanel/ScrollRect/AllChat"));
 
-		int rand = Random.Range (20, 90);
+		/*
+		int rand = Random.Range (2, 3);
 		for (int i = 0; i < rand; i++) {
 			chatManager.Add(new ChatMessage("kabum42", "hey nigga "+i));
 		}
+		*/
+
+		chatInputField = GameObject.Find ("Canvas/ChatPanel/InputField");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		updateChat ();
 
-	
 	}
+
+	void updateChat() {
+
+		if (Input.GetKeyDown(KeyCode.Return)
+			&& lastTimeChatInputFocused
+		    && chatInputField.GetComponent<InputField> ().text != "") {
+
+			string info = chatInputField.GetComponent<InputField> ().text;
+			addChatMessage(new ChatMessage("kabum42", info));
+			chatInputField.GetComponent<InputField> ().text = "";
+			EventSystem.current.SetSelectedGameObject(chatInputField, null);
+			chatInputField.GetComponent<InputField> ().OnPointerClick(new PointerEventData(EventSystem.current));
+
+		}
+		
+		chatManager.Update ();
+
+		lastTimeChatInputFocused = chatInputField.GetComponent<InputField> ().isFocused;
+
+	}
+
+	void addChatMessage(ChatMessage chatMessage) {
+
+		chatManager.Add(chatMessage);
+
+	}
+
 }
 
 
