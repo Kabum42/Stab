5d93c0268d55539bf4afe652cbfb694c07454c25
Kabum42 <kabum42@gmail.com>
2016-03-06 18:57:34 +0100

    Nested cameras

---
+++ Assets/Code/LookAtCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.transform.LookAt (Camera.main.gameObject.transform);
+	}
+
+}
