7a5389d35cfc631f50ea8b65ddeb3c1c7da9f13c
Kabum42 <kabum42@gmail.com>
2016-04-27 01:26:44 +0200

    Changed Shift for Control

--- Assets/Code/RagdollScript.cs
+++ Assets/Code/RagdollScript.cs
@@ -20,7 +20,7 @@ public class RagdollScript : MonoBehaviour {
 
 	void Update() {
 
-		if (Input.GetKey(KeyCode.LeftShift)) {
+		if (Input.GetKey(KeyCode.LeftControl)) {
 			if (Input.GetKeyDown(KeyCode.O)) {
 				Disable();
 			}
